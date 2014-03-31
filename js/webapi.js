e77414b8f7ee1a9566c53f0ed1a89d2ce9be760d
Fernando Jiménez <ferjmoreno@gmail.com>
2014-03-30 20:18:32 -0400

    Using light events

--- js/webapi.js
+++ js/webapi.js
@@ -6,12 +6,17 @@ var WebAPIDemo = {
     // DOM elements.
     this.level = document.getElementById('battery-level');
     this.charging = document.getElementById('battery-charging');
+    this.content = document.getElementById('content');
 
     // Subscribe to battery events.
     navigator.battery.addEventListener('chargingchange',
                                        this.onChargingChange.bind(this));
     navigator.battery.addEventListener('levelchange',
                                        this.onLevelChange.bind(this));
+
+    // Subscribe to device light event.
+    window.addEventListener('devicelight',
+                            this.onDeviceLightChange.bind(this));
 
     // Set initial values.
     this.onChargingChange();
@@ -27,6 +32,13 @@ var WebAPIDemo = {
   onLevelChange: function onLevelChange() {
     var percent = parseInt(navigator.battery.level * 100);
     this.level.textContent = percent + '%';
+  },
+
+  onDeviceLightChange: function onDeviceLightChange(event) {
+    var value = event.value * 2;
+    this.content.style.color = value < 150 ? 'white' : 'black';
+    document.body.style.background =
+      'rgb(' + value + ',' + value + ',' + value + ')';
   }
 
 };
